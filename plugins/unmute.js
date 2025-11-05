d744032b6f3085dd49d4144899c2130114c78b97
1122bot <herokuteam4@gmail.com>
2025-11-05 15:31:41 +0500

    Update unmute.js

--- plugins/unmute.js
+++ plugins/unmute.js
@@ -9,6 +9,6 @@ module.exports = {
   execute: async (sock, msg) => {
     const { remoteJid } = msg.key;
     await sock.groupSettingUpdate(remoteJid, "not_announcement");
-    await sock.sendMessage(remoteJid, { text: "🔊 Group has been unmuted." }, { quoted: msg });
+    await sock.sendMessage(remoteJid, { text: "*YEH GROUP AB OPEN HO CHUKA HAI 😃* \n*AB AP SAB IS GROUP ME CHAT KAR SAKTE HAI 🥰* " }, { quoted: msg });
   }
 };
